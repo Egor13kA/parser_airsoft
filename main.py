b0e76988372a247a8769e6bcc3669f5482c1aeac
Egor13kA <avdoshen@gmail.com>
2021-09-06 22:15:09 +0300

    Наименование получается в 1 итерацию

--- main.py
+++ main.py
@@ -5,22 +5,14 @@ import urllib.request
 req = urllib.request.urlopen('https://air-wars.ru/rashodniki/pirotehnika/')
 html = req.read()
 
-# Получаем тело с карточками товара
+# Получаем soup с карточками товара
 soup = BeautifulSoup(html, 'html.parser')
-body = soup.find_all('div', class_='product-grid row')
 
-for item in body:
-    # title = item.find_all('h4', class_='name')
-    # for i in title:
-    #     grenade_name = i.get_text()
-    #     print(grenade_name)
-    price = item.find_all('div', class_='price')
-    if item.find_all('span', class_='price-new') is True:
-        pass
-    else:
-        for i in price:
-            grenade_price = i.get_text().strip()
-            # print(grenade_price)
+grenade = []
 
-
-
+# Получаем наименование и цену
+for item in soup.find_all('div', class_='col-sm-3 box-product'):
+    title = item.h4.a.get_text()
+    price = item.price
+    print(title)
+    print(price)
