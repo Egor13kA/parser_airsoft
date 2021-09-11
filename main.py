5cfc312d154ced7c2307665b9d1bbe56466e90cf
Egor13kA <avdoshen@gmail.com>
2021-09-11 21:10:57 +0300

    Исправлено: только актуальная цена. Создать: - запись информации в БД. - наличие есть\нет - подгрузку страницы

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ grenade = []
 
 for item in soup.find('div', class_='product-grid row').find_all('div', class_='col-sm-3 box-product'):
     title = item.h4.a.get_text()
+    # Выбор актуальной цены
     if item.find(class_='price-new') == None:
         price = item.find('div', class_='price').get_text(strip=True)
     else:
         price = item.find(class_='price-new').get_text(strip=True)
+
     grenade.append({
        'title': title,
-       'price': price
+       'price': price.replace(' p.', '')
     })
     # print(title)
     # print(price)
